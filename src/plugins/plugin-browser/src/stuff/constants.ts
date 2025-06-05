e38122ea010451665eb64548d4e9de5c37f5aea5
shipwr3ckd <avik7621@gmail.com>
2025-06-06 03:39:37 +0600

    sync local

--- src/plugins/plugin-browser/src/stuff/constants.ts
+++ src/plugins/plugin-browser/src/stuff/constants.ts
@@ -1,7 +1,7 @@
 import constants from "$/constants";
 
 export default {
-	proxyUrl: "https://bn-plugins.github.io/vd-proxy/",
+	proxyUrl: "https://raw.githubusercontent.com/shipwr3ckd/plugin-tests/master/",
 	customLinks: {
 		"vendetta.nexpid.xyz": path =>
 			`${constants.github.url}tree/main/src/plugins/${path.join("/")}`,
